ef5f16bfc469d73053b3cb7e65f481066540d342
jeongan <ijeong-an@ijeong-an-ui-MacBookPro.local>
2022-09-11 19:05:29 +0900

    사용자 검색 정보 SQL에 저장-- 완료

--- routes/search_route.py
+++ routes/search_route.py
@@ -3,24 +3,35 @@ from flask import Flask, render_template, request, redirect, url_for,jsonify
 from flask import Blueprint
 import pymysql
 
+conn = pymysql.connect(host='localhost', user='root', password='Lja15410!',db='cp1', charset='utf8')
+cur = conn.cursor()
+
 bp = Blueprint('search', __name__, url_prefix='/search')
 
 @bp.route('/',methods = ['GET','POST'])
 def search():
     # home 에서 입력받은 값을 받아온다.
-    ingredients = request.form['ingredients']
-    try:
-        ingredients = ingredients.split(",")
-    except:
-        pass
+    ingredients = str(request.form['ingredients'])
     price = request.form['price']
-    category = request.form['category']
-    dislike = request.form['dislike']
-    try:
-        dislike = dislike.split(",")
-    except:
-        pass
+    how = str(request.form['how'])
+    category = str(request.form['category'])
+    difficulty = str(request.form['difficulty'])
+    time = str(request.form['time'])
+    dislike = str(request.form['dislike'])
+
+    print(f"'{ingredients}', {price}, '{how}', '{category}', '{difficulty}', '{time}', '{dislike}'")
+    if ingredients != " ":
+        ingredients = ingredients.strip()
     
+    if dislike != " ":
+        dislike = dislike.strip()
+
+    if price == " ":
+        price = 99999999
+
+    cur.execute(f"INSERT INTO user_search VALUES ('{ingredients}', {price}, '{how}', '{category}', '{difficulty}', '{time}', '{dislike}');")
+    
+    conn.commit()
 
     # sql 쿼리문을 작성하여 fetch
     # conn = pymysql.connect(host='localhost', user='root', password='Lja15410!',db='football', charset='utf8')
@@ -31,10 +42,13 @@ def search():
     # """)
 
     return render_template('search_ing.html', 
-        ingredients= " ".join(ingredients),
+        ingredients=ingredients,
         price=price,
+        how=how,
         category=category,
-        dislike= " ".join(dislike),
+        difficulty=difficulty,
+        time=time,
+        dislike= dislike,
         enumerate=enumerate)
 
 
